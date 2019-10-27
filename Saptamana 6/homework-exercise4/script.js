8edd23a89f7e2fcf0e37a2df0f8d0ae4c282bfef
Botezatu <boteralu@amazon.com>
2019-10-27 15:30:57 +0200

    "modifications made"

--- Saptamana 6/homework-exercise4/script.js
+++ Saptamana 6/homework-exercise4/script.js
@@ -16,13 +16,15 @@ function getPersonGender(CNP) {
     if (typeof CNP !== "string") {
         CNP = CNP.toString();
     }
-    if (CNP[0] === '2') {
-        return "Persoana verificata este de sexul feminin."
-    } else if (CNP[0] === '1') {
-        return "Persoana verificata este de sexul masculin."
-    } else {
-        return "Necunoscut"
+    if (CNP.length === 13) {
+        if (CNP[0] === '2') {
+            return "Persoana verificata este de sexul feminin."
+        } else if (CNP[0] === '1') {
+            return "Persoana verificata este de sexul masculin."
+        }
     }
+
+    return "Necunoscut"
 }
 
 console.log(getPersonGender(2000000));
@@ -35,9 +37,14 @@ console.log(getPersonGender(2000000));
 // - 10 = A+
 // Functia va returna un text de forma "Calificatul corespunzator punctajului [punctaj] este [calificativ]".
 
-var student = function (points) {
+/**
+ * pentru un numar de puncte de la 1 la 10 returnam calificativul aferent sau un mesaj de eroare 
+ * @description returneaza calificativul in functie de puncte
+ * @param {int} points 
+ */
+var getMarkBasedOnPoints = function (points) {
     switch (true) {
-        case points <= 3:
+        case points <=1 && points <= 3:
             return "Calficativul corespunzator punctajului " + points + " este E.";
         case points <= 6:
             return "Calficativul corespunzator punctajului " + points + " este D.";
@@ -51,79 +58,88 @@ var student = function (points) {
             return "Out of range"
     }
 }
-console.log(student(2));
+console.log(getMarkBasedOnPoints(2));
 
 //3 3. Scrieti o functie care accepta ca argument numele marcii unei masini si returneaza un text de forma: "Marca [marca] se produce in [tara]". Trebuie sa aveti macar 6 cazuri. In cazul in care functia nu recunoaste marca, va returna "Marca este necunoscuta. " Scrieti codul in 3 variante diferite. In declararea functiei trebuie sa se regaseasca o functie anonima. Una dintre implementari trebuie sa fie bazata pe Object Literals ( read:  https://ultimatecourses.com/blog/deprecating-the-switch-statement-for-object-literals )
 
-var cars = function (mark) {
+
+
+
+/** 
+ * Avand la dispozitie numele producatorului automobilului obtinem un mesaj in care primim informatii despre 
+ * tara in care este produs automobilul sau in caz contrar un mesaj sugestiv
+ * 
+ * @description Bazat pe numele producatorului automobilului obtinem tara in care este produs acesta
+ * 
+ * @param {string} manufacturer 
+ */
+var getCarOriginSwitch = function (manufacturer) {
 
     switch (true) {
 
-        case mark == "BMW":
-            return "Marca " + mark + " se produce in Germania";
-        case mark == "Ford":
-            return "Marca " + mark + " se produce in Sua";
-        case mark == "Mazda":
-            return "Marca " + mark + " se produce in Japonia";
-        case mark == "Hyundai":
-            return "Marca " + mark + " se produce in Coreea de Sud";
-        case mark == "Dacia":
-            return "Marca " + mark + " se produce in Romania";
-        case mark == "Peugeot":
-            return "Marca " + mark + " se produce in Franta";
+        case manufacturer == "BMW":
+            return "Marca " + manufacturer + " se produce in Germania";
+        case manufacturer == "Ford":
+            return "Marca " + manufacturer + " se produce in Sua";
+        case manufacturer == "Mazda":
+            return "Marca " + manufacturer + " se produce in Japonia";
+        case manufacturer == "Hyundai":
+            return "Marca " + manufacturer + " se produce in Coreea de Sud";
+        case manufacturer == "Dacia":
+            return "Marca " + manufacturer + " se produce in Romania";
+        case manufacturer == "Peugeot":
+            return "Marca " + manufacturer + " se produce in Franta";
         default:
             return "Marca este necunoscuta."
     }
 }
-console.log(cars("Opel"));
+console.log(getCarOriginSwitch("Opel"));
 
-function Mycars(mark) {
-    if (mark == "Opel") {
-        return "Marca " + mark + " se produce in Franta";
-    } else if (mark == "Citroen") {
-        return "Marca " + mark + " se produce in Franta";
-    } else if (mark == "Dodge") {
-        return "Marca " + mark + " se produce in Sua";
-    } else if (mark == Kia) {
-        return "Marca " + mark + " se produce in Japonia";
-    } else if (mark == Renault) {
-        return "Marca " + mark + " se produce in Franta";
-    } else if (mark == Skoda) {
-        return "Marca " + mark + " se produce in Germania";
+function getCarOriginIf(manufacturer) {
+    if (manufacturer == "Opel") {
+        return "Marca " + manufacturer + " se produce in Franta";
+    } else if (manufacturer == "Citroen") {
+        return "Marca " + manufacturer + " se produce in Franta";
+    } else if (manufacturer == "Dodge") {
+        return "Marca " + manufacturer + " se produce in Sua";
+    } else if (manufacturer == Kia) {
+        return "Marca " + manufacturer + " se produce in Japonia";
+    } else if (manufacturer == Renault) {
+        return "Marca " + manufacturer + " se produce in Franta";
+    } else if (manufacturer == Skoda) {
+        return "Marca " + manufacturer + " se produce in Germania";
     }
 
 }
-console.log(Mycars("Opel"));
+console.log(getCarOriginIf("Opel"));
 
-var cars = {
-    countries: {
-        BMW: "Germania",
-        Audi: "Germania",
-        Ford: "Sua",
-        Hyundai: "Coreea de Sud",
-        Mazda: "Japonia",
-        Opel: "Franta"
-    },
-    getMessageSwitch: function (mark) {
-        switch (true) {
-            case this.countries[mark] !== undefined:
-                return "Marca " + mark + " se produce in " + this.countries[mark];
-            default:
-                return "Marca este necunoscuta."
-        }
-    },
-    getMessageIf: function (mark) {
-        if (this.countries[mark] !== undefined) {
-            return "Marca " + mark + " se produce in " + this.countries[mark];
-        }
 
-        return "Marca este necunoscuta.";
+function getCarOriginAdvanced(manufacturer){
+    var carsOrigins = {
+        countries: {
+            BMW: "Germania",
+            Audi: "Germania",
+            Ford: "Sua",
+            Hyundai: "Coreea de Sud",
+            Mazda: "Japonia",
+            Opel: "Franta"
+        },
+        getMessageSwitch: function (manufacturer) {
+            switch (true) {
+                case this.countries[manufacturer] !== undefined:
+                    return "Marca " + manufacturer + " se produce in " + this.countries[manufacturer];
+                default:
+                    return "Marca este necunoscuta."
+            }
+        }
     }
+
+    return carsOrigins.getMessageSwitch(manufacturer)
 }
+console.log(getCarOriginAdvanced("Opel"));
+console.log(getCarOriginAdvanced("BMW"));
+console.log(getCarOriginAdvanced("Unknown"));
 
-
-console.log(cars.getMessageSwitch("Ford"));
-console.log(cars.getMessageIf("BMW"));
 
 //4 4. Scrieti o functie de tip IIFE care:
 // - sorteaza urmatorul array in functie de proprietatea "salary" a fiecarui obiect continut:
@@ -133,6 +149,15 @@ console.log(cars.getMessageIf("BMW"));
 //   { name: "Bekker", salary: 15000 }
 // ];
 // - afiseaza in consola array-ul sortat
+
+/** 
+ * Avand la dispozitie salariile angajatilor creez o functie care sa returneze sortarea acestora crescator
+ * 
+ * @description Bazat pe salariul angajatului facem sortarea 
+ * 
+ * @param {string} employee
+ */
+
 (function (Employee) {
     Employee.sort(function (a, b) {
         if (a.salary > b.salary) {
